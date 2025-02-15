1506e40a3ad1cb6339a59a4f06f0790113a8d20f
amarthya <amarthyaseshu2@gmail.com>
2025-02-15 19:10:38 +0530

    commit

--- src/java8problemsolving/Problems.java
+++ src/java8problemsolving/Problems.java
@@ -13,7 +13,8 @@ public class Problems {
                 new Employee("Alice", "Engineering", 7500, LocalDate.of(2020, 1, 15)),
                 new Employee("Bob", "Sales", 4500, LocalDate.of(2019, 5, 20)),
                 new Employee("Charlie", "Engineering", 8000, LocalDate.of(2021, 3, 10)),
-                new Employee("David", "HR", 6000, LocalDate.of(2018, 11, 5))
+                new Employee("David", "HR", 6000, LocalDate.of(2018, 11, 5)),
+                new Employee("Anil", "HR", 6900, LocalDate.of(2018, 11, 5))
         );
 
 
@@ -81,5 +82,61 @@ public class Problems {
 
         long hr = employees.parallelStream().filter(e -> "HR".equalsIgnoreCase(e.getDepartment())).count();
         System.out.println(hr);
+
+        /*
+        8. Find the highest-paid employee in each department.
+         */
+
+        Map<String, Optional<Employee>> highestPaidByDept = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary))));
+        System.out.println(highestPaidByDept);
+
+        /*
+        9. Avg Salary
+         */
+        Double avgSalary = employees.stream().collect(Collectors.averagingDouble(Employee::getSalary));
+        System.out.println(avgSalary);
+
+        /*
+        10. Get a list of employees whose names start with 'A', sorted by salary in descending order.
+         */
+
+        List<Employee> empASalaryDesc = employees.stream().filter(e -> e.getName().startsWith("A")).sorted(Comparator.comparingDouble(Employee::getSalary).reversed()).collect(Collectors.toList());
+        System.out.println(empASalaryDesc);
+
+        /*
+        11. Create a map where the key is the department name, and the value is the number of employees in that department.
+         */
+
+        Map<String, Long> noOfEmpByDept = employees.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+        System.out.println(noOfEmpByDept);
+
+        /*
+        12.Given a list of transactions (Transaction{id, type, amount}), write a Java 8 program to:
+            Group transactions by type .
+             Calculate the total amount for each transaction type.
+         */
+
+        List<Transaction> transactions = List.of(
+                new Transaction("Food", 50),
+                new Transaction("Grocery", 30),
+                new Transaction("Food", 100),
+                new Transaction("Transport", 20)
+        );
+
+        Map<String, List<Transaction>> transactionsByType = transactions.stream().collect(Collectors.groupingBy(Transaction::getType));
+        System.out.println(transactionsByType);
+
+        Map<String, Double> totalAmtForTransactionType = transactions.stream().collect(Collectors.groupingBy(Transaction::getType, Collectors.summingDouble(Transaction::getAmount)));
+
+        System.out.println(totalAmtForTransactionType);
+
+        /*
+        13. find the second-highest salary
+         */
+
+        Optional<Employee> secondHighest = employees.stream().sorted(Comparator.comparingDouble(Employee::getSalary).reversed()).skip(1).findFirst();
+        System.out.println(secondHighest.get());
+
+
     }
 }
